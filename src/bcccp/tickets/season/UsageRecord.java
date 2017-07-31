f276e6ffea2cbb86475cef7ea5cab3451d61a549
christopher alejandro navarro atarama <christopher.navarro@acu.edu.au>
2017-07-31 14:49:37 +1000

    Update UsageRecord and UsageRecordFactory

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -1,49 +1,41 @@
 package bcccp.tickets.season;
 
 public class UsageRecord implements IUsageRecord {
-	
-	String ticketId;
-	long startDateTime;
-	long endDateTime;
-	
-	
-	
-	public UsageRecord(String ticketId, long startDateTime) {
-		//TODO Implement constructor
-	}
+
+    String ticketId;
+    long startDateTime;
+    long endDateTime;
 
 
-
-	@Override
-	public void finalise(long endDateTime) {
-		// TODO Auto-generated method stub
-		
-	}
+    public UsageRecord(String ticketId, long startDateTime) {
+        this.ticketId = ticketId;
+        this.startDateTime = startDateTime;
+    }
 
 
+    @Override
+    public void finalise(long endDateTime) {
+        this.endDateTime = endDateTime;
 
-	@Override
-	public long getStartTime() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+    }
 
 
-	@Override
-	public long getEndTime() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public long getStartTime() {
+        return startDateTime;
+    }
 
 
+    @Override
+    public long getEndTime() {
+        return endDateTime;
+    }
 
-	@Override
-	public String getSeasonTicketId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	
+
+    @Override
+    public String getSeasonTicketId() {
+        return ticketId;
+    }
+
+
 }
