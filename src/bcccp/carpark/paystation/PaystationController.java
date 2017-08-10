59dc75bca91808b55dd8e1c2289859b44b69e536
Christopher <christopher.navarro@acu.edu.au>
2017-08-10 22:32:45 +1000

    Commit PaystationController.java

--- src/bcccp/carpark/paystation/PaystationController.java
+++ src/bcccp/carpark/paystation/PaystationController.java
@@ -3,45 +3,50 @@ package bcccp.carpark.paystation;
 import bcccp.carpark.ICarpark;
 import bcccp.tickets.adhoc.IAdhocTicket;
 
-public class PaystationController 
-		implements IPaystationController {
-	
-	private IPaystationUI ui;	
-	private ICarpark carpark;
+public class PaystationController implements IPaystationController {
 
-	private IAdhocTicket  adhocTicket = null;
-	private float charge;
-	
-	
+  private IPaystationUI ui;
+  private ICarpark carpark;
 
-	public PaystationController(ICarpark carpark, IPaystationUI ui) {
-		//TODO Implement constructor
-	}
+  private IAdhocTicket adhocTicket = null;
+  private float charge;
 
+  public PaystationController(ICarpark carpark, IPaystationUI ui) {
+    ui.registerController(this);
+    this.carpark = carpark;
+    this.ui = ui;
+    ui.display("Idle");
+  }
 
+  @Override
+  public void ticketInserted(String barcode) {
+    if (carpark.getAdhocTicket(barcode).getEntryDateTime() == adhocTicket.getEntryDateTime()) {
+      charge = carpark.calculateAddHocTicketCharge(adhocTicket.getEntryDateTime());
+      ui.display("AU " + charge);
 
-	@Override
-	public void ticketInserted(String barcode) {
-		// TODO Auto-generated method stub
-		
-	}
+    } else {
+      ui.display("Go to the office");
+    }
+  }
 
+  @Override
+  public void ticketPaid() {
 
+    adhocTicket.pay(adhocTicket.getExitDateTime(), charge);
+    carpark.recordAdhocTicketExit();
+    ui.printTicket(
+        carpark.getName(),
+        adhocTicket.getTicketNo(),
+        adhocTicket.getEntryDateTime(),
+        adhocTicket.getPaidDateTime(),
+        charge,
+        adhocTicket.getBarcode());
+  }
 
-	@Override
-	public void ticketPaid() {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void ticketTaken() {
+      ui.display("Idle");
+      ui.deregisterController();
 
-
-
-	@Override
-	public void ticketTaken() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-	
+  }
 }
